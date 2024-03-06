c92c696225ee4a89926858fb1e4a16c7402b6f41
arash-pa <arash.parsanejad@vasscompany.com>
2024-03-06 14:09:41 +0000

    d

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import "src\index.css";
+import "index.css";
 import TextInput from './components/TextInput';
 //import DateInput from './components/DateInput';
 // ... other imports 
