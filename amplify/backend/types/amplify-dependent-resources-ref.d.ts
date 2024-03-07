41f6a230d363b756353e9aea550e1c56cfe48fda
arash-pa <arash.parsanejad@vasscompany.com>
2024-03-07 10:43:05 +0000

    please please please

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,25 +1,13 @@
 export type AmplifyDependentResourcesAttributes = {
   "api": {
-    "apiGetLookups": {
-      "ApiId": "string",
-      "ApiName": "string",
-      "RootUrl": "string"
-    },
-    "ccsGetLookups": {
+    "getAppianLookups": {
       "ApiId": "string",
       "ApiName": "string",
       "RootUrl": "string"
     }
   },
   "function": {
-    "getLookups": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "todoGetLookups": {
+    "getAppianLookups": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
       "LambdaExecutionRoleArn": "string",
